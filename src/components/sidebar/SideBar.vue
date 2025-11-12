7f1d049177bae6923460939d76db195c6e124ed3
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2025-11-12 15:32:09 +0800

    feat: add backend selection dialog to sidebar

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -29,11 +29,34 @@
         </li>
       </ul>
       <template v-if="isSidebarCollapsed">
-        <VerticalInfos v-if="showStatisticsWhenSidebarCollapsed" />
+        <VerticalInfos v-if="showStatisticsWhenSidebarCollapsed">
+          <div class="flex w-full flex-col items-center justify-center gap-2">
+            <button
+              class="btn btn-circle btn-sm bg-base-300"
+              @click="showBackendSelectorDialog = true"
+              @mouseenter="(e) => showTip(e, t('selectBackend'), { placement: 'right' })"
+            >
+              <ServerIcon class="h-5 w-5" />
+            </button>
+            <button
+              class="btn btn-circle btn-sm bg-base-300"
+              @click="isSidebarCollapsed = false"
+            >
+              <ArrowRightCircleIcon class="h-5 w-5" />
+            </button>
+          </div>
+        </VerticalInfos>
         <div
+          class="flex w-full flex-col items-center justify-center gap-2"
           v-else
-          class="flex w-full items-center justify-center"
         >
+          <button
+            class="btn btn-circle btn-sm bg-base-300"
+            @click="showBackendSelectorDialog = true"
+            @mouseenter="(e) => showTip(e, t('selectBackend'), { placement: 'right' })"
+          >
+            <ServerIcon class="h-5 w-5" />
+          </button>
           <button
             class="btn btn-circle btn-sm bg-base-300"
             @click="isSidebarCollapsed = false"
@@ -49,18 +72,28 @@
         </div>
       </template>
     </div>
+
+    <DialogWrapper
+      v-model="showBackendSelectorDialog"
+      :title="$t('selectBackend')"
+    >
+      <BackendSwitch :disable-edit-backend="true" />
+    </DialogWrapper>
   </div>
 </template>
 
 <script setup lang="ts">
+import DialogWrapper from '@/components/common/DialogWrapper.vue'
+import BackendSwitch from '@/components/settings/BackendSwitch.vue'
 import CommonSidebar from '@/components/sidebar/CommonCtrl.vue'
 import { ROUTE_ICON_MAP, ROUTE_NAME } from '@/constant'
 import { renderRoutes } from '@/helper'
 import { useTooltip } from '@/helper/tooltip'
 import router from '@/router'
 import { isSidebarCollapsed, showStatisticsWhenSidebarCollapsed } from '@/store/settings'
-import { ArrowRightCircleIcon } from '@heroicons/vue/24/outline'
+import { ArrowRightCircleIcon, ServerIcon } from '@heroicons/vue/24/outline'
 import { twMerge } from 'tailwind-merge'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRoute } from 'vue-router'
 import OverviewCarousel from './OverviewCarousel.vue'
@@ -68,6 +101,8 @@ import VerticalInfos from './VerticalInfos.vue'
 
 const { showTip } = useTooltip()
 const { t } = useI18n()
+
+const showBackendSelectorDialog = ref(false)
 
 const mouseenterHandler = (e: MouseEvent, r: string) => {
   if (!isSidebarCollapsed.value) return
